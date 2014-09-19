2354c0c7cc2711c3fd9b80fdcd293e120b6af623
heo jun <gjwns2440@naver.com>
2014-09-19 18:25:26 +0900

    유아이유

--- analyzerwindow.cpp
+++ analyzerwindow.cpp
@@ -1,11 +1,12 @@
 #include "analyzerwindow.h"
 #include "ui_analyzerwindow.h"
-
+#include <QGraphicsView>
 analyzerWindow::analyzerWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::analyzerWindow)
 {
     ui->setupUi(this);
+
 }
 
 analyzerWindow::~analyzerWindow()
